8417574b25f2696074fa012c04e44370dc2d3a40
Henrique87645 <125613025+Henrique87645@users.noreply.github.com>
2024-06-19 08:05:12 -0300

    verde.ino

--- verde.ino
+++ verde.ino
@@ -1,28 +1,41 @@
+/*Posições vetor:
+  [0] = esquerda
+  [1] = centro
+  [2] = direita
+  [3] = direita 90
+  [4] = esquerda 90
+*/
 void Verde() {
  ///ler o valor do LDR
- //O valor lido será entre 0 e 1023
-
-  // beco sem saida (dois verdes ativados)
-  if ((analogRead(ldrEsq) > verdeVal) && (analogRead(ldrDir) > verdeVal)) { 
-    while(digitalRead(IR[1]) == 1){  
-     moverEsquerda(V);
-      delay(1500);
-    }
-  }
+ //O valor lido será entre 0 e 1023 
+  ldrEsq = analogRead(ldrEsquerdo1);
+  ldrDir = analogRead(ldrDireito1);
+  Serial.println(analogRead(ldrEsquerdo1));
+  Serial.println( analogRead(ldrDireito1));
 
   //notação verde na esquerda
-  if ((analogRead(ldrEsq) > verdeVal) && (analogRead(ldrDir) < verdeVal)) { 
-    while(digitalRead(IR[1]) == 1){  
-       moverEsquerda90(V);
-       delay(10);
+  if ((ldrEsq > verdeVal) && (ldrDir < verdeVal)) { 
+    if((digitalRead(IR[4]) == 0) && (digitalRead(IR[0]) == 0)) {
+      Serial.println("Verde esquerda");
+      moverEsquerda90(V);
+      delay(20);
     }
   } 
 
-   //notação verde na direita
-  if ((analogRead(ldrEsq) < verdeVal) && (analogRead(ldrDir) > verdeVal)) { 
-    while(digitalRead(IR[1]) == 1){  
-       moverDireita90(V);
-       delay(10);
+  //notação verde na direita
+  if ((ldrEsq < verdeVal) && (ldrDir > verdeVal)) { 
+    if((digitalRead(IR[3]) == 0) && (digitalRead(IR[2]) == 0)) {
+      Serial.println("Verde Direita");
+      moverDireita90(V);
+      delay(20);
     }
   } 
+  //beco sem saida (dois verdes ativados)
+  if ((analogRead(ldrEsq) > verdeVal) && (analogRead(ldrDir) > verdeVal)) { 
+    if((digitalRead(IR[3]) == 0) && (digitalRead(IR[4]) == 0)){
+      Serial.println("beco verde");  
+      moverTras(V);
+      delay(15);
+    }
+  }
 }
