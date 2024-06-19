02cb311ccc9dd7e0923e36214bbd5d7a0aadaee1
Henrique87645 <125613025+Henrique87645@users.noreply.github.com>
2024-06-19 08:04:49 -0300

    curva_de_90.ino

--- curva_de_90.ino
+++ curva_de_90.ino
@@ -2,30 +2,32 @@
   [0] = esquerda
   [1] = centro
   [2] = direita
-  [3] = direita 90
-  [4] = esquerda 90
+  [3] = esquerda 90
+  [4] = direita 90
 */
 
 //LEMBRETE QUE 0-LUZ LIGADA E 1-DESLIGADA
 void Curva90() {
 
-// COMEÇO CURVA DE 90 DIREITA
-      if ((digitalRead(IR[2]) == 0) && (digitalRead(IR[3]) == 0)) {
-        while((digitalRead(IR[1]) == 1) && (digitalRead(IR[4]) == 1)){
+      // COMEÇO CURVA DE 90 DIREITA
+      if ((digitalRead(IR[4]) == 0)&& (digitalRead(IR[3]) == 1) ) {
+        //Enquanto seu valor estiver ligado, ele gira para a direita
+        while ((digitalRead(IR[1]) == 0)) {
           Serial.println("90 direita");
-          moverDireita90(255);
-          delay(10);
+          moverDireita90(V);
+          delay(20);
         }
-      } // FIM CURVA 90 DIREITA 
+      V = 100;
+      } 
 
       // COMEÇO CURVA DE 90 ESQUERDA
-      if ((digitalRead(IR[0]) == 0) && (digitalRead(IR[4]) == 0)) {
-        while((digitalRead(IR[1]) == 1) && (digitalRead(IR[3]) == 1)){
+      if ((digitalRead(IR[3]) == 0) && (digitalRead(IR[4]) == 1)  ) {
+        //Enquanto seu valor estiver ligado, ele gira para a esquerda
+        while ((digitalRead(IR[1]) == 0)) {
           Serial.println("90 esquerda");
-          moverEsquerda90(255);
-          delay(10);
+          moverEsquerda90(V);
+          delay(20);
         }
-      } //FIM CURVA 90 ESQUERDA 
-      
-      Seguelinha();
+      V = 100;
+      }
 }
